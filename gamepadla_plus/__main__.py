a065a8951fe35f207a4f0c853ff2ac5673ed3d64
Adam McKellar <dev@mckellar.eu>
2024-11-02 14:03:34 +0100

    refactor: split up the test command function

--- gamepadla_plus/__main__.py
+++ gamepadla_plus/__main__.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import os
 from enum import Enum
 
@@ -23,11 +24,17 @@ LICENSE_FILE_NAME = "LICENSE.txt"
 
 
 class StickSelector(str, Enum):
-    left = "left"
-    right = "right"
+    LEFT = "left"
+    RIGHT = "right"
 
 
-class FailedTest(Exception):
+class GamePadConnection(str, Enum):
+    CABLE = "Cable"
+    BLUETOOTH = "Bluetooth"
+    DONGLE = "Dongle"
+
+
+class GamepadlaError(Exception):
     pass
 
 
@@ -76,13 +83,146 @@ def filter_outliers(array: list[float]) -> list[float]:
     return sorted_array[lower_index : upper_index + 1]
 
 
-def test_execution(samples: int, stick: StickSelector, id: int) -> dict:
+def test_execution(
+    samples: int,
+    stick: StickSelector,
+    joystick: JoystickType,
+    tick: Callable[[float], None],
+) -> dict:
     """
     Executes the testing algorithm.
 
     Pygame NEEDS to be initalized firstm.
     """
-    pass
+    joystick.init()  # Initialize the selected joystick
+    joystick_name = joystick.get_name()
+
+    if stick == StickSelector.LEFT:
+        axis_x = 0  # Axis for the left stick
+        axis_y = 1
+    elif stick == StickSelector.RIGHT:
+        axis_x = 2  # Axis for the right stick
+        axis_y = 3
+
+    if not joystick.get_init():
+        raise GamepadlaError("Controller not connected")
+
+    times: list[float] = []
+    delay_list: list[float] = []
+    start_time: float = time.time()
+    prev_x: float | None = None
+    prev_y: float | None = None
+
+    # Main loop to gather latency data from joystick movements
+    while len(times) < samples:
+        pygame.event.pump()
+        x = joystick.get_axis(axis_x)
+        y = joystick.get_axis(axis_y)
+        pygame.event.clear()
+
+        # Ensure the stick has moved significantly (anti-drift)
+        if not ("0.0" in str(x) and "0.0" in str(y)):
+            if prev_x is None and prev_y is None:
+                prev_x, prev_y = x, y
+            elif x != prev_x or y != prev_y:
+                end_time = time.time()
+                start_time = end_time
+                prev_x, prev_y = x, y
+
+                while True:
+                    pygame.event.pump()
+                    new_x = joystick.get_axis(axis_x)
+                    new_y = joystick.get_axis(axis_y)
+                    pygame.event.clear()
+
+                    # If stick moved again, calculate delay
+                    if new_x != x or new_y != y:
+                        end = time.time()
+                        delay = round((end - start_time) * 1000, 2)
+                        if delay != 0.0 and delay > 0.2 and delay < 150:
+                            times.append(delay * 1.057)  # Adjust for a 5% offset
+                            tick(delay)
+                            delay_list.append(delay)
+
+                        break
+
+    # Filter outliers from delay list
+    delay_clear = delay_list
+    delay_list = filter_outliers(delay_list)
+
+    # Calculate statistical data
+    filteredMin = min(delay_list)
+    filteredMax = max(delay_list)
+    filteredAverage = np.mean(delay_list)
+    filteredAverage_rounded = round(filteredAverage, 2)
+
+    polling_rate = round(1000 / filteredAverage, 2)
+    jitter = round(np.std(delay_list), 2)
+
+    os_name = platform.system()
+    max_polling_rate = get_polling_rate_max(polling_rate)
+    stablility = round((polling_rate / max_polling_rate) * 100, 2)
+
+    return {
+        "joystick_name": joystick_name,
+        "os_name": os_name,
+        "max_polling_rate": max_polling_rate,
+        "polling_rate": polling_rate,
+        "stablility": stablility,
+        "filteredMin": filteredMin,
+        "filteredAverage_rounded": filteredAverage_rounded,
+        "filteredMax": filteredMax,
+        "jitter": jitter,
+        "delay_clear": delay_clear,
+    }
+
+
+def wrap_data_for_server(result: dict) -> dict:
+    """
+    Wraps the test result struct into another struct for compatiblity.
+    """
+    stamp = uuid.uuid4()
+    uname = platform.uname()
+    os_version = uname.version
+
+    return {
+        "test_key": str(stamp),
+        "version": __version__,
+        "url": "https://gamepadla.com",
+        "date": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+        "driver": result["joystick_name"],
+        "os_name": result["os_name"],
+        "os_version": os_version,
+        "min_latency": result["filteredMin"],
+        "avg_latency": result["filteredAverage_rounded"],
+        "max_latency": result["filteredMax"],
+        "polling_rate": result["polling_rate"],
+        "jitter": result["jitter"],
+        "mathod": "GP",
+        "delay_list": ", ".join(map(str, result["delay_clear"])),
+    }
+
+
+def upload_data(data: dict, connection: GamePadConnection, name: str) -> bool:
+    """
+    Uploads results to server.
+    """
+    # Add connection and gamepad name to the data
+    data["connection"] = connection.value
+    data["name"] = name
+
+    # Send test results to the server
+    response = requests.post("https://gamepadla.com/scripts/poster.php", data=data)
+
+    return response.status_code == 200
+
+
+def write_to_file(data: dict, path: str):
+    """
+    Writes result to file.
+    """
+    with open(path, "w") as outfile:
+        json.dump(data, outfile, indent=4)
 
 
 app = typer.Typer(
@@ -108,13 +248,17 @@ def list():
 
 @app.command()
 def test(
-    out: str = typer.Option(help="Write result to file.", default=""),
+    out: str | None = typer.Option(help="Write result to file.", default=None),
     samples: int = typer.Option(help="How many samples are to be taken.", default=2000),
     stick: StickSelector = typer.Option(
-        help="Choose which stick to test with.", default=StickSelector.left
+        help="Choose which stick to test with.", default=StickSelector.LEFT
     ),
     upload: bool = typer.Option(
         help="Upload result to <gamepadla.com>?", default=False
+    ),
+    gamepad_name: str | None = typer.Option(help="Name of the game pad", default=None),
+    gamepad_connection: GamePadConnection | None = typer.Option(
+        help="How the game pad is connected.", default=None
     ),
     id: int = typer.Argument(
         help="Controller id. Check possible controllers with list command.", default=0
@@ -124,6 +268,10 @@ def test(
     Test controller with id.
     """
 
+    if upload and (gamepad_name is None or gamepad_connection is None):
+        rprint("[red]Upload requires to set gamepad-name and gamepad-connection![/red]")
+        exit(1)
+
     pygame.init()
 
     joysticks = get_joysticks()
@@ -132,157 +280,60 @@ def test(
         exit(1)
     joystick = joysticks[id]
 
-    joystick.init()  # Initialize the selected joystick
-    joystick_name = joystick.get_name()
-
-    if stick == StickSelector.left:
-        axis_x = 0  # Axis for the left stick
-        axis_y = 1
-    elif stick == StickSelector.right:
-        axis_x = 2  # Axis for the right stick
-        axis_y = 3
-
-    if not joystick.get_init():
-        rprint("[red]Controller not connected[/red]")
-        exit(1)
-
-    times: list[float] = []
-    delay_list: list[float] = []
-    start_time: float = time.time()
-    prev_x: float | None = None
-    prev_y: float | None = None
-
-    # Main loop to gather latency data from joystick movements
     with tqdm(
         total=samples,
         ncols=76,
         bar_format="{l_bar}{bar} | {postfix[0]}",
         postfix=[0],
     ) as pbar:
-        while True:
-            pygame.event.pump()
-            x = joystick.get_axis(axis_x)
-            y = joystick.get_axis(axis_y)
-            pygame.event.clear()
 
-            # Ensure the stick has moved significantly (anti-drift)
-            if not ("0.0" in str(x) and "0.0" in str(y)):
-                if prev_x is None and prev_y is None:
-                    prev_x, prev_y = x, y
-                elif x != prev_x or y != prev_y:
-                    end_time = time.time()
-                    start_time = end_time
-                    prev_x, prev_y = x, y
+        def progress_bar_update(delay: float):
+            pbar.update(1)
+            pbar.postfix[0] = "{:05.2f} ms".format(delay)
 
-                    while True:
-                        pygame.event.pump()
-                        new_x = joystick.get_axis(axis_x)
-                        new_y = joystick.get_axis(axis_y)
-                        pygame.event.clear()
-
-                        # If stick moved again, calculate delay
-                        if new_x != x or new_y != y:
-                            end = time.time()
-                            delay = round((end - start_time) * 1000, 2)
-                            if delay != 0.0 and delay > 0.2 and delay < 150:
-                                times.append(delay * 1.057)  # Adjust for a 5% offset
-                                pbar.update(1)
-                                pbar.postfix[0] = "{:05.2f} ms".format(delay)
-                                delay_list.append(delay)
-
-                            break
-
-                if len(times) >= samples:
-                    break
-
-    # Filter outliers from delay list
-    delay_clear = delay_list
-    delay_list = filter_outliers(delay_list)
-
-    # Calculate statistical data
-    filteredMin = min(delay_list)
-    filteredMax = max(delay_list)
-    filteredAverage = np.mean(delay_list)
-    filteredAverage_rounded = round(filteredAverage, 2)
-
-    polling_rate = round(1000 / filteredAverage, 2)
-    jitter = round(np.std(delay_list), 2)
-
-    os_name = platform.system()
-    max_polling_rate = get_polling_rate_max(polling_rate)
-    stablility = round((polling_rate / max_polling_rate) * 100, 2)
+        result = test_execution(
+            samples=samples, stick=stick, joystick=joystick, tick=progress_bar_update
+        )
 
     rprint(
         Markdown(
             f"""
 | Parameter           | Value                         |
 |---------------------|-------------------------------|
-| Gamepad mode        | {joystick_name}               |
-| Operating System    | {os_name}                     |
-| Polling Rate Max.   | {max_polling_rate} Hz         |
-| Polling Rate Avg.   | {polling_rate} Hz             |
-| Stability           | {stablility}%                 |
+| Gamepad mode        | {result["joystick_name"]}     |
+| Operating System    | {result["os_name"]}                     |
+| Polling Rate Max.   | {result["max_polling_rate"]} Hz         |
+| Polling Rate Avg.   | {result["polling_rate"]} Hz             |
+| Stability           | {result["stablility"]}%                 |
 |                     |                               |
-| Minimal latency     | {filteredMin} ms              |
-| Average latency     | {filteredAverage_rounded} ms  |
-| Maximum latency     | {filteredMax} ms              |
-| Jitter              | {jitter} ms                   |
+| Minimal latency     | {result["filteredMin"]} ms              |
+| Average latency     | {result["filteredAverage_rounded"]} ms  |
+| Maximum latency     | {result["filteredMax"]} ms              |
+| Jitter              | {result["jitter"]} ms                   |
 """
         )
     )
 
-    stamp = uuid.uuid4()
-    uname = platform.uname()
-    os_version = uname.version
+    data = wrap_data_for_server(result=result)
 
-    data = {
-        "test_key": str(stamp),
-        "version": __version__,
-        "url": "https://gamepadla.com",
-        "date": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-        "driver": joystick_name,
-        "os_name": os_name,
-        "os_version": os_version,
-        "min_latency": filteredMin,
-        "avg_latency": filteredAverage_rounded,
-        "max_latency": filteredMax,
-        "polling_rate": polling_rate,
-        "jitter": jitter,
-        "mathod": "GP",
-        "delay_list": ", ".join(map(str, delay_clear)),
-    }
-
-    if out != "":
-        with open(out, "w") as outfile:
-            json.dump(data, outfile, indent=4)
-        rprint(f"[green]Wrote result to file {out}[/green]")
+    if out is not None:
+        try:
+            write_to_file(data=data, path=out)
+            rprint(f"[green]Wrote result to file {out}[/green]")
+        except Exception as e:
+            rprint(f"[red]Failed to write result to path {out}.[/red]")
+            raise e
 
     if upload:
-        gamepad_name = input("Please enter the name of your gamepad: ")
-        connection = input(
-            "Please select connection type (1. Cable, 2. Bluetooth, 3. Dongle): "
-        )
-        if connection == "1":
-            connection = "Cable"
-        elif connection == "2":
-            connection = "Bluetooth"
-        elif connection == "3":
-            connection = "Dongle"
-        else:
-            rprint("Invalid choice. Defaulting to Cable.")
-            connection = "Unset"
+        try:
+            upload_data(data=data, connection=gamepad_connection, name=gamepad_name)
 
-        # Add connection and gamepad name to the data
-        data["connection"] = connection
-        data["name"] = gamepad_name
-
-        # Send test results to the server
-        response = requests.post("https://gamepadla.com/scripts/poster.php", data=data)
-        if response.status_code == 200:
             rprint("[green]Test results successfully sent to the server.[/green]")
+            stamp = data["test_key"]
             webbrowser.open(f"https://gamepadla.com/result/{stamp}/")
-        else:
+        except Exception as e:
             rprint("[red]Failed to send test results to the server.[/red]")
+            raise e
 
 
 @app.command()
